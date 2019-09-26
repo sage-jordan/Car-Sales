430fe7ccdd15363b061b0fd7cd1fed1f2a5e8174
sage-jordan <sagemjrodan@gmail.com>
2019-09-26 02:47:55 -0400

    import featureReducer and const store

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { createStore } from "redux"
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { featureReducer } from './reducers/featureReducer';
+
+const store = createStore(featureReducer)
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
